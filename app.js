39ae787bcf4af35c57af3c6f10dec50cc944859c
Doorsz12 <109147995+Doorsz12@users.noreply.github.com>
2024-11-25 20:47:46 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -21,3 +21,26 @@ setInterval(Animate, 16000);
 about.addEventListener('click', function() {
     document.getElementById("tep1").scrollIntoView({behavior: "smooth"});
 });
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let zmienna = 0;
+btn1.onclick = () => {
+    if(zmienna == 0 || zmienna == 2 || zmienna == 4) {
+        zmienna++;
+    }
+    else if(zmienna == 5) {
+        zmienna = 0;
+        window.open("logowanie.php");
+    } 
+    else {
+        zmienna = 0;
+    }
+}
+btn2.onclick = () => {
+    if(zmienna == 1 || zmienna == 3) {
+        zmienna++;
+    }
+    else {
+        zmienna = 0;
+    }
+}
---
+++ logowanie.php
@@ -0,0 +1,18 @@
+<?php 
+    echo "nigger";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>???</title>
+</head>
+<body>
+    <center>
+        <h2>what are you even doing here wtf</h2>
+        <input type="text">
+        <button>verify</button>
+    </center>
+</body>
+</html>
